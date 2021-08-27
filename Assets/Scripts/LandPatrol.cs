c35321d7e06118a04c8f791fe5f5191a08749251
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-08-27 08:01:00 +0500

    init

---
+++ Assets/Scripts/LandPatrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandPatrol : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _distance;
+    [SerializeField] private Transform _groundDetector;
+
+    private bool _isMovingRight = true;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * _speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(_groundDetector.position, Vector2.down, _distance);
+
+        if (groundInfo.collider == false)
+        {
+            if (_isMovingRight == true)
+            {
+                MyCodeShit(0, -180, 0, false);
+            }
+            else
+            {
+                MyCodeShit(0, 0, 0, true);
+            }
+        }
+    }
+
+    private void MyCodeShit(int x, int y, int z, bool resualt)
+    {
+        transform.eulerAngles = new Vector3(x, y, z);
+        _isMovingRight = resualt;
+    }
+}
---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Animator))]
+public class PlayerMovment : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _raycatsDistance;
+    [SerializeField] private LayerMask _groundLayerMask;
+
+    private Rigidbody2D _r2b2;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _r2b2 = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        var movevment = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movevment, 0, 0) * Time.deltaTime * _speed;
+
+        _animator.SetFloat("Speed", Mathf.Abs(movevment));
+
+        if (GroundCheck() && Input.GetButtonDown("Jump"))
+        {
+            _r2b2.AddForce(Vector2.up * _jumpForce);
+        }
+    }
+
+    private bool GroundCheck()
+    {
+        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, Vector2.down, _raycatsDistance, _groundLayerMask);
+        return raycastHit.collider != null;
+    }
+}
